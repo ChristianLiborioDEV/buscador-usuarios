ee4de10a25791adcfee3caeb64b11da430c8f8ed
Christian Liborio <christianliborio.dev@gmail.com>
2026-08-12 19:30:13 -0300

    Tratamento de erro e mensagem de retorno div

--- app.js
+++ app.js
@@ -4,29 +4,42 @@ const divLoading = document.getElementById("loading");
 const divErro = document.getElementById("erro");
 const divUsuario = document.getElementById("card");
 
-const mostrar = (elemento, mensagem) => {
+const mostrar = (elemento) => {
     elemento.classList.remove("escondido");
 }
 
-const esconder = (elemento, mensagem) => {
+const esconder = (elemento) => {
     elemento.classList.add("escondido");
 }
 
+const mostrarUsuario = (elemento, arrayUsuarios) => {
+    elemento.classList.remove("escondido");
+    elemento.innerHTML = ` 
+    <h2>Nome: ${arrayUsuarios.name}</h2>
+    <p><b>Username:</b> ${arrayUsuarios.username}<p>
+    <p><b>Email:</b> ${arrayUsuarios.email}<p>
+    <p><b>Telefone:</b> ${arrayUsuarios.phone}<p>
+    <p><b>Site:</b> ${arrayUsuarios.website}<p>
+    `
+}
 
 const buscarUsuario = async (id) => {
     mostrar(divLoading)
     try{
         const buscaApi = await fetch (`https://jsonplaceholder.typicode.com/users/${id}`);
         if(!buscaApi.ok) {
-            throw new Error
+            throw new Error(`Usuário não encontrado — status: ${buscaApi.status}`)
         }
         const resultado = await buscaApi.json();
         esconder(divLoading)
-        mostrar(divUsuario, `${resultado.name}`)
+        mostrarUsuario(divUsuario, resultado)
+
         console.log(resultado)
+
     } catch (erro){
-        esconder(divLoading)
-        mostrar(divErro)
+        esconder(divLoading);
+        divErro.querySelector("h2").textContent = erro.message;
+        mostrar(divErro);
     }
 } 
 
